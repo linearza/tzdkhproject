1a32ff7aa0138d0f8d1e10f61bd9da3969b6a663
Michiel <michiel.vanwijk@timbuktutravel.com>
2016-11-22 22:13:19 +0200

    Ajax form

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,44 @@
-(function ($, window, document, undefined) {
+(function($, window, document, undefined) {
 
   'use strict';
 
-  $(function () {
-    // FireShell
+  $(function() {
+
+    // Contact form
+    var $contactForm = $('#entry-form'),
+      $submitButton = $('#entry-form input[type=submit]');
+    $contactForm.submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '//formspree.io/vanwijk.mc@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $submitButton.prop('disabled', true);
+          $submitButton.before('<div class="alert sending">Sending message…</div>');
+        },
+        success: function() {
+          $contactForm.find('.alert.sending').remove();
+          $submitButton.before('<div class="alert sent">Message sent!</div>');
+          $submitButton.prop('disabled', false);
+          setTimeout(function() {
+            $contactForm.find('.alert.sent').remove();
+          }, 5000);
+        },
+        error: function() {
+          $contactForm.find('.alert.sending').remove();
+          $submitButton.before('<div class="alert error">Oops, there was an error. Please contact me directly at daniella@mylittleswimschool.co.za</div>');
+          $submitButton.prop('disabled', false);
+          setTimeout(function() {
+            $contactForm.find('.alert.error').remove();
+          }, 10000);
+        }
+      });
+    });
+
+
+
   });
 
 })(jQuery, window, document);
